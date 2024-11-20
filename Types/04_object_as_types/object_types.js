86b966c3d69edb8ba512daf191a8088894fb9b59
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-11-20 17:19:41 +0100

    04-object-types

---
+++ Types/04_object_as_types/object_types.js
@@ -0,0 +1,6 @@
+"use strict";
+let vehicle = {
+    brand: "Mercedes",
+    engineType: 5.0,
+};
+vehicle.brand = "";
---
+++ Types/04_object_as_types/object_types.ts
@@ -0,0 +1,6 @@
+let vehicle: { brand: string; engineType: number } = {
+  brand: "Mercedes",
+  engineType: 5.0,
+};
+
+vehicle.brand = "";
