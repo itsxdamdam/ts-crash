fb15f61ee7a610380e3d784b07b7fe1713b8f6cf
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2024-11-20 15:15:58 +0100

    02-types-2

---
+++ Types/02_types2.ts
@@ -0,0 +1,20 @@
+let cname: any;
+
+cname = 12;
+cname = "Idan";
+
+// let cars = ["Toyota", "BMW", "KIA", "Mercedes"];
+
+// cars = ["Audi"];
+
+// let cars: any[] = ["Toyota", "BMW", "KIA", "Mercedes"];
+
+// cars = ["Audi"];
+
+// let cars: any = ["Toyota", "BMW", "KIA", "Mercedes"];
+
+// cars = "Audi";
+
+let cars: number[] = [2, 45, 6, 56];
+
+cars = [4556];
