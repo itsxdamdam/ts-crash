d015a95429be1fd89bd2cbde7506886c3e0858a0
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-03-04 21:18:40 +0100

    namespace intro

--- namespace/module1.js
+++ namespace/module1.js
@@ -1,3 +1,4 @@
+"use strict";
 var myNameSpace;
 (function (myNameSpace) {
     myNameSpace.name = "Maria";
@@ -7,5 +8,3 @@ var myNameSpace;
     myNameSpace.displayData = displayData;
 })(myNameSpace || (myNameSpace = {}));
 console.log(myNameSpace.displayData());
-///<reference path="module1.ts"/>
-// purpose of namespace is protecting data from global scope and keeping your modules seperately. Namespace is like a module enclosed with a name and can be referred to using the name.
--- namespace/namespace.ts
+++ namespace/namespace.ts
@@ -1,5 +1,3 @@
-namespace myNameSpace {
-  function displayData() {
-    console.log("Hi, my name is idan");
-  }
-}
+///<reference path="module1.ts"/>
+
+// purpose of namespace is protecting data from global scope and keeping your modules seperately. Namespace is like a module enclosed with a name and can be referred to using the name.
