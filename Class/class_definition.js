560616e8082908810bc8b34ad4aa016983e79eab
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-03-04 19:43:14 +0100

    feat: class modifiers

--- Class/class_definition.js
+++ Class/class_definition.js
@@ -1,8 +1,19 @@
 "use strict";
 class Tree {
     // branch: string;
-    constructor(branch) {
-        this.branch = branch;
-        this.branch = branch;
+    constructor(leaf) {
+        this.leaf = leaf;
+        this.leaf = leaf;
+    }
+    // access modifiers
+    // public: gives access outside of class
+    // private: it is not available outside the class
+    // protected: it is only available when you use another child of the existing class to extend 
+    // methods
+    moveLeaf() {
+        this.leaf = "neu neu leaf";
+        console.log(`The ${this.leaf} is moving to the right`);
     }
 }
+let tree1 = new Tree("Green leaf");
+tree1.moveLeaf();
--- Class/class_definition.ts
+++ Class/class_definition.ts
@@ -1,7 +1,23 @@
 class Tree {
   // branch: string;
 
-  constructor(public branch: string) {
-    this.branch = branch;
+  constructor(public leaf: string) {
+    this.leaf = leaf;
+  }
+
+  // access modifiers
+  // public: gives access outside of class
+  // private: it is not available outside the class
+  // protected: it is only available when you use another child of the existing class to extend 
+
+  // methods
+
+  public moveLeaf() {
+    this.leaf = "neu neu leaf";
+    console.log(`The ${this.leaf} is moving to the right`);
   }
 }
+
+let tree1 = new Tree("Green leaf");
+
+tree1.moveLeaf();
