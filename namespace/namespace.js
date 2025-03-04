2bfcd8e2ff49330c3650805c55edf209a656f34e
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-03-04 21:23:14 +0100

    namespace alias

--- namespace/namespace.js
+++ namespace/namespace.js
@@ -8,4 +8,6 @@ var myNameSpace;
 })(myNameSpace || (myNameSpace = {}));
 console.log(myNameSpace.displayData());
 ///<reference path="module1.ts"/>
+var thisSpace = myNameSpace.displayData;
+console.log(thisSpace);
 // purpose of namespace is protecting data from global scope and keeping your modules seperately. Namespace is like a module enclosed with a name and can be referred to using the name.
--- namespace/namespace.ts
+++ namespace/namespace.ts
@@ -1,3 +1,8 @@
 ///<reference path="module1.ts"/>
 
+import thisSpace = myNameSpace.displayData
+
+console.log(thisSpace);
+
+
 // purpose of namespace is protecting data from global scope and keeping your modules seperately. Namespace is like a module enclosed with a name and can be referred to using the name.
